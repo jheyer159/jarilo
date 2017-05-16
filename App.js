80a1dedfa07666b7f93f758adfb6916b78de398f
Jeremy Heyer <jheyer159@gmail.com>
2017-05-15 19:32:48 -1000

    Updated app to include proper ticking.

--- App.js
+++ App.js
@@ -1,62 +1,68 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react"
+import { StyleSheet, Text, View } from "react-native"
 
 export default class App extends Component {
-  render() {
-    return (
+	render() {
+		return (
       <View style={styles.container}>
         <SensorData name="Humidity" data="24"/>
         <SensorData name="Air temp" data="56" />
         <GetData />
       </View>
-    );
-  }
+		)
+	}
 }
 
 class SensorData extends Component {
-  render(){
-    return (
+	render(){
+		return (
       <Text>{this.props.name}: {this.props.data}</Text>
-    )
-  }
+		)
+	}
 }
 
 class GetData extends Component {
-  constructor(props){
-    super(props)
-    this.state = { data: false }
-    let pollRateSec = 4
+	constructor(props){
+		super(props)
+		this.state = { data: false }
+	}
 
-    setInterval(() => {
-      //this.setState( { data: !this.state.data } )
-      pullDht()
-    }, pollRateSec * 1000)
-    
-    async function pullDht() {
-      try {
-        console.log("got into async func")
-        let response = await fetch('http://192.168.0.103:8888');
-        let responseJson = await response.json()
-        //this.setState( { data: !this.state.data } )
-      } catch(error) {
+	componentDidMount() {
+		let tickRateSec = 2
+		this.timerID = setInterval(
+      () => this.tick(),
+      tickRateSec * 1000
+    )
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID)
+	}
+
+  async tick() {
+    try {
+      console.log("got into async func")
+      let response = await fetch('http://localhost:8888');
+      let responseJson = await response.json()
+      console.log( responseJson )
+      this.setState( {data: JSON.stringify(responseJson)})
+    } catch(error) {
         console.error(error);
-      }
     }
   }
 
-
-  render(){
-    return(
+	render(){
+		return(
       <Text>State: {this.state.data.toString()} </Text>
-    )
-  }
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	}
+})
